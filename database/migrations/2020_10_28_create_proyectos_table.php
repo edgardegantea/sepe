9d9d2cfffd2d25e13b2f6203d262d28e7ddbeb9a
DalilaAGtz <71537871+DalilaAGtz@users.noreply.github.com>
2020-10-27 20:45:20 -0600

    Create 2020_10_28_create_proyectos_table.php
    Se creo la tabla proyectos con algunas caracteristicas principales

---
+++ database/migrations/2020_10_28_create_proyectos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProyectosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proyectos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100)->unique();
+            $table->string('descripcion', 200);
+            $table->string('desarrollador', 200);
+            $table->decimal('semestre',1);
+            $table->string('asesor', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proyectos');
+    }
+}
