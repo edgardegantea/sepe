1673f671cdc6ecdca195c3312236f64dcdc74f72
EliseoGuanicoL <Chengsluya_1.2>
2021-01-09 19:10:30 -0600

    Actualizacion Parte1

---
+++ database/migrations/2021_01_09_222735_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstName', 100);
+            $table->string('lastName', 100);
+            $table->integer('semester');
+            $table->string('engineering', 50);
+            $table->string('controlNumber', 15);
+            $table->string('email', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
---
+++ database/migrations/2021_01_09_222824_create_teachers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstName', 70);
+            $table->string('lastName', 100);
+            $table->string('controlNumber', 15);
+            $table->string('email', 100);
+            $table->integer('idProject');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+}
